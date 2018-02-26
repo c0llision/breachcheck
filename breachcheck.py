141c0d3ad3ce913df37555c851b050acba4dac6e
c0llision <0xmw@protonmail.com>
2018-02-26 16:18:43 +0000

    move to seperate class

--- breachcheck.py
+++ breachcheck.py
@@ -1,44 +1,15 @@
 #!/usr/bin/env python3
-import requests
-import hashlib
 from getpass import getpass
-
-
-def query_hibp(pw):
-    ''' returns number of times password appeared in dataset
-    returns 0 if password was not found'''
-    pw_hash = hashlib.sha1(pw.encode()).hexdigest().upper()
-    short_hash = pw_hash[:5]
-
-    r = requests.get('https://api.pwnedpasswords.com/range/' + short_hash)
-
-    if r.status_code != 200:
-        raise Exception('Error occurred quering server, got HTTP status code:',
-                        r.status_code)
-
-    found = False
-    for line in r.text.split():
-        breached_hash, breach_count = line.split(':')
-        if short_hash + breached_hash == pw_hash:
-            found = True
-            break
-
-    if not found:
-        breach_count = 0
-    else:
-        breach_count = max(1, int(breach_count))
-
-    return breach_count
+from hibpcheck import hibpcheck
 
 
 def main():
     password = getpass()
-    breach_count = query_hibp(password)
-
-    if breach_count > 0:
+    check = hibpcheck(password)
+    if check.found:
         print('WARNING: password was FOUND!')
         print('This password appeared in the dataset ' +
-              str(breach_count) + " times")
+              str(check.count) + " times")
     else:
         print('Password was not found in the dataset')
 
---
+++ hibpcheck.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import requests
+import hashlib
+
+
+class hibpcheck():
+    def __init__(self, pw):
+        self.count = 0
+        self.found = False
+        self.password(pw)
+
+    def password(self, pw):
+        self.pw = pw
+        self.found = False
+
+        pw_hash = hashlib.sha1(pw.encode()).hexdigest().upper()
+        short_hash = pw_hash[:5]
+
+        r = requests.get('https://api.pwnedpasswords.com/range/' + short_hash)
+
+        if r.status_code != 200:
+            raise Exception(
+                'Error occurred quering server, got HTTP status code:',
+                r.status_code)
+
+        for line in r.text.split():
+            breached_hash, breach_count = line.split(':')
+            if short_hash + breached_hash == pw_hash:
+                self.found = True
+                break
+
+        if self.found:
+            self.count = int(breach_count)
+        else:
+            self.count = 0
+
+        return self.found
