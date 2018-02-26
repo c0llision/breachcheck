5cbe5322733381242b575a79a790b1b3c0feeb9f
c0llision <0xmw@protonmail.com>
2018-02-26 14:42:23 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Breach Check
+
+Checks if entered password appears in the HaveIBeenPwned dataset of leaked passwords.
+Uses k-Anonymity to avoid exposing the password to HIBP servers.
---
+++ breachcheck.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import requests
+import hashlib
+from getpass import getpass
+
+
+def query_hibp(pw):
+    ''' gets list of pwned hashes from hibp'''
+    pw_hash = hashlib.sha1(pw.encode()).hexdigest().upper()
+    r = requests.get('https://api.pwnedpasswords.com/range/' + pw_hash[:5])
+
+    if r.status_code != 200:
+        raise Exception('Error occurred quering server, got HTTP status code:',
+                        r.status_code)
+
+    found = False
+    for line in r.text.split():
+        breached_hash, breach_count = line.split(':')
+        if breached_hash in pw_hash:
+            found = True
+            break
+
+    if found:
+        print('WARNING: password was FOUND!')
+        print('This password appeared in the dataset ' +
+              breach_count + " times")
+    else:
+        print('Password was not found')
+
+
+def main():
+    password = getpass()
+    query_hibp(password)
+
+
+if __name__ == '__main__':
+    main()
