40fe296f361858527a48fc661d71ef85c0f29f19
c0llision <0xmw@protonmail.com>
2018-04-30 00:23:52 +0100

    update test code

--- main.py
+++ main.py
@@ -4,21 +4,11 @@ from hibpcheck import hibpPassword, hibpEmail
 
 
 def main():
-    # email = input('Email:')
-    # hbp = hibpEmail(email)
-    # if hbp.found:
-    #    print("found")
-    #    print (str(hbp.count))
-    
-    password = getpass()
-    hibp = hibpPassword(password)
-    if hibp.found:
-        print('WARNING: password was FOUND!')
-        print('This password appeared in the dataset ' +
-              str(hibp.count) + " times")
-    else:
-        print('Password was not found in the dataset')
-
+    email = input('Email:')
+    hbp = hibpEmail(email)
+    if hbp.found:
+        print("found")
+        print (str(hbp.count))
 
 if __name__ == '__main__':
     main()
